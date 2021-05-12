b479b902b617b5263298be07b6b7644a495dcdae
Fernanda Frers <fernanda.frers@gmail.com>
2021-05-11 19:06:16 -0500

    models and connection

---
+++ models/index.js
@@ -0,0 +1,32 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
+// Categories have many Products
+Category.belongsTo(Product, {
+    foreignKey: 'category_id'
+});
+// Products belongToMany Tags (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    as: 'product_tag',
+    foreignKey: 'tag_id'
+});
+// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+    though: ProductTag,
+    as: 'product_tag',
+    foreignKey: 'product_id'
+})
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
